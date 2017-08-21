4298314d14be352b9b05ed59fb3fc17d78d12554
onkbear <onkbear@gmail.com>
2017-08-21 17:19:14 +0900

    Add flags

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 
 var Transform = require('readable-stream/transform');
 
-module.exports = function (search, replacement, options) {
+module.exports = function (options) {
+  if (options === undefined) {
+    options = {}
+  }
   return new Transform({
     objectMode: true,
     transform: function appendQueryStringTransform(file, enc, cb) {
@@ -16,10 +19,17 @@ module.exports = function (search, replacement, options) {
         return new Buffer(String(contents).replace(search, replacement));
       }
 
+      const isCSS = 'css' in options ? options.css : true;
+      const isJS = 'js' in options ? options.js : true;
+
       if (file.isBuffer()) {
         const query = Math.random().toString(36).slice(-8);
-        file.contents = appendQueryString(file.contents, 'css', query);
-        file.contents = appendQueryString(file.contents, 'js', query);
+        if (isCSS) {
+          file.contents = appendQueryString(file.contents, 'css', query);
+        }
+        if (isJS) {
+          file.contents = appendQueryString(file.contents, 'js', query);
+        }
       }
 
       return cb(null, file);
