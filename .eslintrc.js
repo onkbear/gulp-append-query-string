3a943e5d6d47e04b93f7eda099ef8ad3a5d9e764
onkbear <onkbear@gmail.com>
2017-08-23 23:58:45 +0900

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "standard",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-console": 0
+  },
+  "globals": {
+  }
+};
